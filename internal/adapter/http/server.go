5001f38ef9ac3c7ca8f82b89906eb19256844026
Hasdanit <230547@astanait.edu.kz>
2025-01-28 12:36:22 +0500

    Server

--- internal/adapter/http/server.go
+++ internal/adapter/http/server.go
@@ -11,3 +11,13 @@ import (
 type Request struct {
 	Name string `json:"name"`
 }
+
+func greetHandler(w http.ResponseWriter, r *http.Request) {
+	var req Request
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	response := fmt.Sprintf("Hello, %s!", req.Name)
+	w.Write([]byte(response))
+}
