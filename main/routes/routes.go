a36ca019ba6d4e5c43e81f4d3877f58733af9eb5
aaaether <bekaaliyev848007@gmail.com>
2025-02-04 11:57:22 +0500

    Deezer API

--- main/routes/routes.go
+++ main/routes/routes.go
@@ -10,9 +10,13 @@ import (
 func RegisterRoutes() *mux.Router {
 	router := mux.NewRouter()
 
+	// Аутентификация
 	router.HandleFunc("/api/register", controllers.Register).Methods("POST")
 	router.HandleFunc("/api/login", controllers.Login).Methods("POST")
 	router.Handle("/api/delete", middleware.AuthMiddleware(http.HandlerFunc(controllers.DeleteUser))).Methods("DELETE")
+
+	// Deezer API
+	router.HandleFunc("/api/deezer/search", controllers.SearchDeezerTrack).Methods("GET")
 
 	return router
 }
