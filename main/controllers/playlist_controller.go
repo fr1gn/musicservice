a96d259db608778f9a3d01f479d70c6cd694513e
Hasdanit <230547@astanait.edu.kz>
2025-02-06 16:26:40 +0500

    controller

--- main/controllers/playlist_controller.go
+++ main/controllers/playlist_controller.go
@@ -3,12 +3,12 @@ package controllers
 import (
 	"encoding/json"
 	"musicservice/main/database"
-	. "musicservice/main/models"
+	"musicservice/main/models"
 	"net/http"
 )
 
 func CreatePlaylist(w http.ResponseWriter, r *http.Request) {
-	var playlist Playlist
+	var playlist models.Playlist
 	json.NewDecoder(r.Body).Decode(&playlist)
 
 	_, err := database.DB.Exec("INSERT INTO playlists (name, user_id) VALUES ($1, $2)", playlist.Name, playlist.UserID)
@@ -22,7 +22,7 @@ func CreatePlaylist(w http.ResponseWriter, r *http.Request) {
 
 func GetPlaylists(w http.ResponseWriter, r *http.Request) {
 	userID := r.URL.Query().Get("user_id")
-	var playlists []Playlist
+	var playlists []models.Playlist
 
 	err := database.DB.Select(&playlists, "SELECT * FROM playlists WHERE user_id=$1", userID)
 	if err != nil {
