92144c734fbb76580bdc71b3264e92b96d24808a
fr1gn <ggtairlan95@gmail.com>
2025-02-13 01:38:53 +0500

    updated

--- main/models/song.go
+++ main/models/song.go
@@ -1,4 +1,9 @@
 package models
 
 type Song struct {
+	ID       int    `json:"id"`
+	Title    string `json:"title"`
+	Artist   string `json:"artist"`
+	Album    string `json:"album"`
+	Duration int    `json:"duration"`
 }
