dd1fce00588ffe1f1a152e578a9f646ed6977ebd
fr1gn <ggtairlan95@gmail.com>
2025-02-18 22:33:42 +0500

    Resolved merge conflicts

--- backend/main/models/song.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Song struct {
-	ID       int    `json:"id"`
-	Title    string `json:"title"`
-	Artist   string `json:"artist"`
-	Album    string `json:"album"`
-	Duration int    `json:"duration"`
-}
